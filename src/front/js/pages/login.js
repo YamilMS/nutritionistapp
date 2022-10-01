1ba40956fbb08d9a46a10b2f086a6d1bcb426e7b
YamilMS <yoms.tf@gmail.com>
2022-10-01 10:20:56 +0000

    first steps doing the login and signup pages

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div>
+      <h2 className="mx-auto w-50 mt-5">Login</h2>
+      <form className="mx-auto w-50">
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-group form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
