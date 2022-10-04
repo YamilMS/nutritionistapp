0fefec45f62e8c8a02643cb30263893904740d12
YamilMS <yoms.tf@gmail.com>
2022-10-04 19:13:08 +0000

    linked home buttons

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,21 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Logo</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-danger">Login</button>
-					</Link>
-					<Link to="/demo">
-						<button className="btn btn-warning">SignIn</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Logo</span>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/login">
+            <button className="btn btn-danger">Login</button>
+          </Link>
+          <Link to="/signUp">
+            <button className="btn btn-warning">Sign Up</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
