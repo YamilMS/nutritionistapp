c175530f75fca0bbfda26b50e0284a2e976e2aa6
Ferreira-Gerson <gersonferreira90@gmail.com>
2022-10-06 18:52:27 +0000

    Profile page

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -12,6 +12,7 @@ export const Profile = () => {
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
 		</div>
