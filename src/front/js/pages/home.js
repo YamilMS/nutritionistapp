1bc243733a90fea0d9b123d68d5147f48909f32b
tomasherman77 <tomasxherman@gmail.com>
2022-10-06 17:53:44 +0000

    logo&sessionsChanges

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -13,8 +13,8 @@ export const Home = () => {
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 				<div class="d-flex justify-content-center align-items-end">	
 				<hr class="my-4"></hr>
-					<a href="#" className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Nutritionist</a>
-					<a href="#" className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Patient</a>
+					<a href="#" className="btn btn-success btn-lg active m-2" role="button" aria-pressed="true">Nutritionist</a>
+					<a href="#" className="btn btn-danger btn-lg active m-2" role="button" aria-pressed="true">Patient</a>
 				</div>
 			</div>
 		</div>
