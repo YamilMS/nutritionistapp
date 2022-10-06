e4e6c5cb0e2161b07517b7122477f4b534ca94d5
Ferreira-Gerson <gersonferreira90@gmail.com>
2022-10-06 18:03:07 +0000

    Profile and Layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -8,6 +8,7 @@ import { Sessions } from "./pages/sessions";
 import { Single } from "./pages/single";
 import { Login } from "./pages/login";
 import { Signup } from "./pages/signup";
+import { Profile } from "./pages/profile";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -30,6 +31,7 @@ const Layout = () => {
             <Route element={<Sessions />} path="/sessions" />
             <Route element={<Login />} path="/login" />
             <Route element={<Signup />} path="/signup" />
+            <Route element={<Profile />} path="/profile" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
---
+++ src/front/js/pages/profile.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import "../../styles/home.css";
+
+export const Profile = () => {
+
+	return (
+		<div className="text-center container">
+			<div className="card">
+            <img className="card-img-top" src="..." alt="Profile Image Card"/>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+		</div>
+	);
+};
